2e5a782f8070a318d4a20b7aca61f20afa9e7aff
zhangtengfei03 <zhangtengfei03@baidu.com>
2020-04-16 11:51:01 +0800

    2020-4-15-修改代码命名

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import myHeader from './reception/myHeader'
 import myFooter from './reception/myFooter'
 import frontLogin from './reception/frontLogin'
-import infor from './reception/infor'
+import info from './reception/info'
 import thirdParty from './reception/thirdParty'
 import updatePwd from './reception/updatePwd'
 import baseLogin from './common/baseLogin'
@@ -11,7 +11,7 @@ export default {
     myHeader,
     myFooter,
     frontLogin,
-    infor,
+    info,
     thirdParty,
     updatePwd,
     baseLogin,

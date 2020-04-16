f455b406f8d32cabdeca2d3181d0aca5f138b0ac
zhangtengfei03 <zhangtengfei03@baidu.com>
2020-04-16 11:48:50 +0800

    2020-4-15-修改代码命名

--- src/api/apipath.js
+++ src/api/apipath.js
@@ -1,12 +1,16 @@
+/**
+ * 加载后端接口名
+ */
+
 import homePage from './reception/homePage'
 import article from './reception/article'
 import common from './common/common'
 import album from './reception/album'
 import leaveMessage from './reception/leaveMessage'
-import maArticle from './backstage/maarticle'
-import maAlbum from './backstage/maalbum'
-import maExhibit from './backstage/maexhibit'
-import maType from './backstage/matype'
+import maArticle from './backstage/maArticle'
+import maAlbum from './backstage/maAlbum'
+import maExhibit from './backstage/maExhibit'
+import maType from './backstage/maType'
 export default {
     homePage,
     article,
